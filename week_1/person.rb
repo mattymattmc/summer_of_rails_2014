66e06ec3e55eeae3fbb578c502d167ff5be821e8
Matt McIntire <mattymattmc@gmail.com>
2014-06-10 00:02:18 +0000

    hashes and arrays

---
+++ week_1/person.rb
@@ -0,0 +1,34 @@
+again = ""
+age = 0
+first_name = ""
+last_name = ""
+loop_again = true
+person = {}
+people = []
+
+while(loop_again) do
+  
+  puts "What is the person's first name?"
+  first_name = gets.chop
+  
+  puts "What is the person's last name?"
+  last_name = gets.chop
+  
+  puts "What is the person's age?"
+  age = gets.chop
+  
+  person = {"first_name" => "#{first_name}", "last_name" => "#{last_name}", "age" => "#{age}"}
+  
+  people.push person
+  # same as people << person
+  
+  puts "Would you like to enter another person (enter no to stop)?"
+  
+  again = gets.chop
+  if again == "no" || again == "NO" || again == "No"
+    loop_again = false
+  end
+
+end
+puts "You entered #{people.length} people."
+
