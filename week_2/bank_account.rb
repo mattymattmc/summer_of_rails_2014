15c4f3cd0133f21d31be741f6678e8d584cbc4f4
Matt McIntire <mattymattmc@gmail.com>
2014-06-20 20:12:45 +0000

    summary method try it

--- week_2/bank_account.rb
+++ week_2/bank_account.rb
@@ -19,8 +19,22 @@ class BankAccount
   def withdrawal(amount)
     @balance -= amount
   end
+  
+  def name=(new_name)
+    @name = new_name
+  end
 
+  def name
+    @name
+  end
+  
+  def summary
+    puts "The account \"#{self.name}\" has a balance of #{self.to_s}."
+  end
+  
 end
+
+=begin
 
 account = BankAccount.new
 puts account.balance
@@ -34,3 +48,11 @@ account2 = BankAccount.new
 
 account2.withdrawal 20
 puts account2
+
+=end
+
+account = BankAccount.new
+account.deposit 20
+account.deposit 20
+account.name = "TestAccount"
+puts account.summary
