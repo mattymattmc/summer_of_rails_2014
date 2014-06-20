91f114bec6444841361960c32415ed4f0ac678e0
Matt McIntire <mattymattmc@gmail.com>
2014-06-20 19:30:56 +0000

    first 'try it' with init and withdrawal

--- week_2/bank_account.rb
+++ week_2/bank_account.rb
@@ -1,18 +1,36 @@
 class BankAccount
 
-def balance
-  @balance = 0 if @balance.nil?
-end
-
-def to_s
-  sprintf("$%.2f", self.balance)
-end
-
-def deposit(amount)
-  @balance += amount
-end
-
-end
-
+  def balance
+    @balance
+  end
   
+  def initialize
+    @balance = 0
+  end
 
+  def to_s
+   sprintf("$%.2f", self.balance)
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+  
+  def withdrawal(amount)
+    @balance -= amount
+  end
+
+end
+
+account = BankAccount.new
+puts account.balance
+
+account.deposit 20
+account.deposit 20
+account.deposit 40.0
+puts account.balance
+
+account2 = BankAccount.new
+
+account2.withdrawal 20
+puts account2
